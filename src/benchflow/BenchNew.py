0cc1907587f4ae8fc04521a8974c8b3a278bbec0
kirk <kirk@benchmarkthing.com>
2025-02-07 15:35:18 -0800

    feat: integrate bff

--- src/benchflow/BenchNew.py
+++ src/benchflow/BenchNew.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import logging
+import sys
 from typing import Dict, Any, List, Union
 from requests.exceptions import HTTPError
 import base64
@@ -17,111 +18,121 @@ class Bench:
         self.benchmark_name = benchmark_name
         self.bff_url = bff_url
         self.benchflow_token = benchflow_token
+        self.version = "0.1.5"
+        self.polling_interval = 20
 
-    def run(self, task_ids: List[Union[str, int]], 
-            agents: Union[BaseAgent, List[BaseAgent]], 
-            requirements_dir: str, 
+    def run(self, agents: Union[BaseAgent, List[BaseAgent]], 
+            requirements_dir: str = None, 
             install_sh: str = None, 
             api: Dict[str, str] = None, 
             require_gpu: bool = False, 
-            params: Dict[str, Any] = {}):
-        
-        if isinstance(task_ids, (str, int)):
+            params: Dict[str, Any] = {},
+            task_ids: Union[str|int, List[str|int], None] = None):
+        if isinstance(task_ids, str|int):
             task_ids = [str(task_ids)]
-        else:
-            task_ids = [str(task) for task in task_ids]
-
         if isinstance(agents, BaseAgent):
             agents = [agents]
         
-        results_ids = []
+        job_ids = []
         try:
             for agent in agents:
-                result_id = self._send_tasks_to_bff(task_ids, agent, requirements_dir, install_sh, api, require_gpu, params)
-                if result_id:
-                    results_ids.append(result_id)
+                job_id = self._send_tasks_to_bff(task_ids, agent, requirements_dir, install_sh, api, require_gpu, params)
+                if job_id:
+                    job_ids.append(job_id)
 
-            return results_ids
+            return job_ids
 
         except Exception as e:
             logger.error(f"Error running benchmark: {str(e)}")
-            return results_ids
+            return job_ids
 
     def _send_tasks_to_bff(self, task_ids: List[str], agent: BaseAgent, 
                            requirements_dir: str, install_sh_dir: str, 
                            api: Dict[str, str], require_gpu: bool, 
                            params: Dict[str, Any]):
         logger.info(f"Sending tasks {task_ids} and setup scripts to BFF for agent {agent.__class__.__name__}")
-
         try:
-            with open(requirements_dir, 'r') as f:
-                requirements_txt = f.read()
-        except Exception as e:
-            logger.error(f"Failed to read requirements.txt: {str(e)}")
-            requirements_txt = ""
-
-        install_sh = None
-        if install_sh_dir:
-            try:
-                with open(install_sh_dir, 'r') as f:
-                    install_sh = f.read()
-            except Exception as e:
-                logger.error(f"Failed to read install.sh: {str(e)}")
-                install_sh = ""
-
-        try:
+            requirements_txt = self._read_file_content(requirements_dir)
+            install_sh = self._read_file_content(install_sh_dir)
             agent_code = self._get_agent_code(agent)
-        except Exception as e:
-            logger.error(f"Failed to get agent code: {str(e)}")
-            agent_code = ""
 
-        payload = {
-            "task_ids": task_ids,
-            "benchmark_name": self.benchmark_name,
-            "params": params,
-            "require_gpu": require_gpu,
-            "requirements_txt": encode_base64(requirements_txt),
-            "install_sh": encode_base64(install_sh),
-            "agent_code": encode_base64(agent_code),
-            "api": api
-        }
+            payload = {
+                "task_ids": task_ids,
+                "benchmark_name": self.benchmark_name,
+                "params": params,
+                "require_gpu": require_gpu,
+                "requirements_txt": encode_base64(requirements_txt),
+                "install_sh": encode_base64(install_sh),
+                "agent_code": encode_base64(agent_code),
+                "api": api
+            }
 
-        headers = {
-            "Authorization": f"Bearer {self.benchflow_token}"
-        }
+            headers = {
+                "x-bf-api-key": self.benchflow_token,
+                "x-bf-source": f"python-sdk {self.version}",
+                "Content-Type": "application/json"
+            }
 
-        try:
-            response = requests.post(f"{self.bff_url}/tasks/run", json=payload, headers=headers)
+            response = requests.post(f"{self.bff_url}/v1/jobs/{self.benchmark_name}/new", headers=headers, json=payload)
             response.raise_for_status()
 
-            task_info = response.json()
-            run_id = task_info.get("run_id")
-            logger.info(f"Tasks {task_ids} started successfully, run_id: {run_id}")
-            return run_id
+            job_info = response.json()
+            job_id = job_info.get("jobId")
+            logger.info(f"Tasks {task_ids} started successfully, job_id: {job_id}")
+            return job_id
         
         except HTTPError as e:
             logger.error(f"Task execution failed: {str(e)}")
+            error_detail = response.json()['detail']
+            logger.error(f"Task execution failed: {error_detail}")
         except Exception as e:
             logger.error(f"Task execution failed: {str(e)}")
         return None
 
-    def get_results(self, run_ids: List[str]):
-        payload = {"run_ids": run_ids}
+    def get_results(self, job_ids: List[str]):
         headers = {
-            "Authorization": f"Bearer {self.benchflow_token}"
+            "x-bf-api-key": self.benchflow_token,
+            "x-bf-source": f"python-sdk {self.version}",
+            "Content-Type": "application/json"
         }
 
-        try:
-            response = requests.post(f"{self.bff_url}/tasks/results", json=payload, headers=headers)
-            response.raise_for_status()
+        completed_jobs = {}
+        pending_jobs = set(job_ids)
 
-            results = response.json()
-            pretty_results = json.dumps(results, indent=4, ensure_ascii=False)
-            print(pretty_results)
-            return results
+        while pending_jobs:
+            for job_id in list(pending_jobs):
+                try:
+                    response = requests.get(f"{self.bff_url}/v1/jobs/{job_id}/", headers=headers)
+                    response.raise_for_status()
+
+                    job_data = response.json().get("job", {})
+                    job_status = job_data.get("status", "UNKNOWN")
+                    
+                    if job_status in ["COMPLETED", "FAILED"]:
+                        completed_jobs[job_id] = job_data
+                        pending_jobs.remove(job_id)
+                        logger.info(f"Job {job_id} completed with status: {job_status}")
+
+                except requests.HTTPError as e:
+                    logger.error(f"HTTP error while fetching job {job_id}: {str(e)}")
+                except Exception as e:
+                    logger.error(f"Unexpected error while fetching job {job_id}: {str(e)}")
+
+            if pending_jobs:
+                time.sleep(self.polling_interval)
+
+        pretty_results = json.dumps(completed_jobs, indent=4, ensure_ascii=False)
+        print(pretty_results)
+        return completed_jobs
+    
+    def _read_file_content(self, file_path: str) -> str:
+        if not file_path:
+            return None
         
-        except HTTPError as e:
-            logger.error(f"Failed to get results: {str(e)}")
-        except Exception as e:
-            logger.error(f"Failed to get results: {str(e)}")
-        return []
+        with open(file_path, 'r') as f:
+            return f.read()
+    
+    def _get_agent_code(self, agent: BaseAgent) -> str:
+        agent_file = sys.modules[agent.__class__.__module__].__file__
+        with open(agent_file, 'r') as f:
+            return f.read()
